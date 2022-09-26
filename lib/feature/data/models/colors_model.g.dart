a416ea832cf770738ac856c5e7e330f14c1c0db7
AlexandrMcQuen <kushnerev7201@yandex.ru>
2022-09-26 16:47:34 +0300

    last update

---
+++ lib/feature/data/models/colors_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'colors_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Colors _$ColorsFromJson(Map<String, dynamic> json) => Colors(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      code: json['code'] as String,
+    );
+
+Map<String, dynamic> _$ColorsToJson(Colors instance) => <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'code': instance.code,
+    };
